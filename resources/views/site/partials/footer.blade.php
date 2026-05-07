e84651ca898907510c444450ab760d1b0bf85502
KenionWS <info@kenion.com.ar>
2026-05-07 16:23:39 -0300

    Update footer credit

--- resources/views/site/partials/footer.blade.php
+++ resources/views/site/partials/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="site-footer">
     <div class="site-footer-inner">
         <span class="site-footer-copy">© 2026 Montepio Antiguedades. Todos los derechos reservados.</span>
-        <a href="{{ $footerBackUrl }}" class="site-footer-link">{{ $footerBackLabel }}</a>
+        <a href="https://kenion.com.ar" class="site-footer-credit" target="_blank" rel="noopener">
+            <span>Desarrollado por</span>
+            <img src="{{ $siteBase }}/assets/brand/kenion-logo.png" alt="Kenion" class="site-footer-credit-logo">
+        </a>
     </div>
 </footer>
